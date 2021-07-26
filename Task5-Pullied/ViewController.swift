46d8d7e6690420cd8d08aaec875437ca8cceba1e
Yuta iwasaki <yanyong0127@gmail.com>
2021-07-27 07:18:59 +0900

    Initial Commit

---
+++ Task5-Pullied/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task5-Pullied
+//
+//  Created by IwasakIYuta on 2021/07/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
